dff2c5e5d88668a64754dea09b9dbb1835550ff1
victorm27 <victormanuel272791@gmail.com>
2024-03-06 19:50:55 -0500

    portafolio-v1

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let typed = new Typed(".text", {
+        strings: ["Desarrollador Backend", "Desarrollador Frontend", "Desarrollador Fullstack"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    }
+    );
+});
+
+
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    // sticky header
+
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+
+    // animation footer on scroll
+
+}
